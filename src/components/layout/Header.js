13208b05e6a7df8c1faccf45669a36c0eb43e063
samrooodriguez <sam27rodriguez@gmail.com>
2019-10-30 15:51:43 -0500

    added dark mode capability, still playing with css to have a solid color scheme

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FaPizzaSlice} from "react-icons/all";
 
-export const Header = () => {
-    return (<header className="header" data-testid="header">
+export const Header = ({darkMode, setDarkMode}) => {
+    const [shouldShowMain, setShouldMain] = useState(false);
+    const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+
+
+
+
+    return <header className="header" data-testid="header">
         <nav>
             <div className="logo">
-                <img src="/images/logo.png" alt="todoist" />
+                <img src="/images/logo.png" alt="todoist"/>
             </div>
             <div className="settings">
                 <ul>
 
                     <li data-testid="quick-add-task-action" className="settings__add">+</li>
-                    <li data-testid="dark-mode-action" className="settings__darkmode"><FaPizzaSlice/></li>
+                    <li
+                        data-testid="dark-mode-action"
+                        className="settings__darkmode"
+                        onClick={() => setDarkMode(!darkMode)}
+                    >
+                        <FaPizzaSlice/></li>
                 </ul>
             </div>
         </nav>
-        </header>
-    );
+    </header>;
 };
